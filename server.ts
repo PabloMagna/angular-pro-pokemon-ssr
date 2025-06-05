fd1e05ec8555701fc3457ed70d6d17742aeb6107
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2025-06-05 15:13:13 -0300

    server.ts corrected2

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 // server.ts
+
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context';
+import { getContext } from '@netlify/angular-runtime/context.mjs';
 
 const angularAppEngine = new AngularAppEngine();
 
@@ -8,6 +9,12 @@ export async function netlifyAppEngineHandler(
   request: Request
 ): Promise<Response> {
   const context = getContext();
+
+  // Ejemplo opcional de endpoint API
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
 
   const result = await angularAppEngine.handle(request, context);
   return result || new Response('Not found', { status: 404 });
